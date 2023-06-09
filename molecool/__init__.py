370a7391bba9c9fc3df23433259b4b27b4ea3031
Denver <dhaycock@nd.edu>
2023-06-09 13:24:10 -0400

    Import everything

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,7 +1,8 @@
 """Python package to visualize molecules given their cartesian coordinates"""
 
 # Add imports here
-from .functions import canvas
+from .functions import *
+# just a new line for a git thing
 
 
-from ._version import __version__
+# from ._version import __version__
