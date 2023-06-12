0918c58ca7ca05d5aeb0ea064c5a9c7c0a0e1667
djhaycock <dhaycock@nd.edu>
2023-06-12 14:21:05 -0400

    updated the function in init

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,7 +1,14 @@
 """Python package to visualize molecules given their cartesian coordinates"""
 
 # Add imports here
-from .functions import canvas
+from .functions import *
+from molecool.measure import calculate_angle, calculate_distance
+
+from molecool.atom_data import atom_colors, atomic_weights
+
+from molecool.visualize import bond_histogram, draw_molecule
+
+
 # just a new line for a git thing
 
 
